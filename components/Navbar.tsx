fa6309ce177bd83d500e7c6f372a9899fb2ccf00
Oskar Sköld <oskar.skold4@hotmail.com>
2023-10-19 06:46:59 +0200

    Adding a more responsive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,41 +1,76 @@
+'use client';
 import Link from 'next/link';
 import React from 'react';
+import { useState } from 'react';
 
 const Navbar = () => {
-  return (
-    <nav>
-      <div className="flex items-center justify-between mx-20 my-10">
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  }
+ return (
+  <nav className="p-4">
+      <div className="flex items-center justify-between p-4">
         <div>
-          <Link href="/" className="text-2xl font-bold">
-            Arsenal
-          </Link>
+          <a href="#" className="text-black text-3xl font-bold">Arsenal</a>
         </div>
-        <div className=" hidden lg:flex lg:items-center lg:justify-between">
-          <Link href="/" className="text-2xl font-bold m-2">
-            PRODUCTS
-          </Link>
-          <Link href="/" className="text-2xl font-bold m-2">
-            ABOUT US
-          </Link>
-        </div>
-        <svg
-          className=" lg:hidden cursor-pointer"
-          fill="none"
-          height="2em" width="2em"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-          
-            d="M4 6h16M4 12h16M4 18h16"
-          ></path>
-        </svg>
-        <div className="hidden lg:flex lg:items-center lg:justify-between">
-          <Link href="/" className="text-xl font-bold m-2">
-            <svg viewBox="0 0 1024 1024" fill="currentColor" height="2em" width="2em">
-              <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0011.6 0l43.6-43.5a8.2 8.2 0 000-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z" />
+        <div className="flex md:hidden">
+          <button
+            onClick={toggleNavbar}
+            className="text-black focus:outline-none"
+          >
+            <svg
+              className="h-10 w-10"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
             </svg>
+          </button>
+        </div>
+        <div className="hidden md:flex items-center">
+          <Link href="/">
+            <p className="text-black text-2xl font-semibold hover:text-gray-700 cursor-pointer mr-4">Products</p>
+          </Link>
+          <Link href="/">
+            <p className="text-black text-2xl font-semibold hover:text-gray-700 cursor-pointer mr-4">About us</p>
+          </Link>
+        </div>
+        <div className="hidden md:flex">
+          <button className="text-black focus:outline-none">
+            <svg
+              className="h-10 w-10"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div className={`md:hidden ${isOpen ? 'block' : 'hidden'}`}>
+        <div className="flex flex-col items-center">
+          <Link href="/">
+            <p className="text-black font-semibold hover:text-gray-700 cursor-pointer p-1">Products</p>
+          </Link>
+          <Link href="/">
+            <p className="text-black font-semibold hover:text-gray-700 cursor-pointer p-1">About us</p>
           </Link>
         </div>
       </div>
