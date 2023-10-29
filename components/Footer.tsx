41bd9215a7063e69693a5d45bf6cdebe0fb79ce6
Oskar Sköld <oskar.skold@clowid.com>
2023-10-29 21:20:58 +0100

    test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { SiteConfig } from '@/types';
-import Link from 'next/link';
 
 type FooterProps = {
   config: SiteConfig | undefined;
@@ -10,9 +9,9 @@ const Footer = ({ config }: FooterProps) => {
   return (
     <footer className="flex flex-col items-start justify-between mt-10 py-12 bg-gray-300">
       <div>
-        <Link href="/" className="text-4xl font-bold ml-12">
+        <a className="text-4xl font-bold ml-12" href="/">
           {config?.footerText}
-        </Link>
+        </a>
       </div>
       <div className="flex items-center justify-between mt-4 ml-12">
         {config?.primaryNavigation?.map((item) => (
@@ -21,9 +20,7 @@ const Footer = ({ config }: FooterProps) => {
             className="block text-black text-xl text-center mr-4 hover:underline"
           >
             {item.route?.accessibleSlug?.current && (
-              <Link key={item._key} href={item.route.accessibleSlug.current}>
-                {item.title}
-              </Link>
+              <a href={item.route.accessibleSlug.current}>{item.title}</a>
             )}
           </div>
         ))}
