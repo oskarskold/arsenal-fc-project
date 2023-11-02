e7cded4a1cb3eb08d89641e41b31aaa9a16cdd2b
Oskar Sköld <oskar.skold4@hotmail.com>
2023-11-02 10:52:25 +0100

    Fixed preview in sanity for all documents

--- sanity/desk/defaultDocumentNode.ts
+++ sanity/desk/defaultDocumentNode.ts
@@ -6,6 +6,8 @@ interface AccessibleSlug {
 }
 
 interface SanityDocument {
+  slug: any;
+  _type: string;
   _id: string;
   accessibleSlug?: AccessibleSlug;
   slugPrefix?: string; // Add this if it is required
@@ -19,17 +21,42 @@ const siteUrl =
   process.env.NODE_ENV === 'development'
     ? `http://localhost:3000/${PREVIEW_PATH}`
     : process.env.NEXT_PUBLIC_SANITY_DATASET === 'production'
-    ? `https://arsenal-fc-project-348a.vercel.app/${PREVIEW_PATH}` //TODO: When page is deployed and connected to domain this should be the live domain-name
+    ? `https://arsenal-fc-project-348a.vercel.app/${PREVIEW_PATH}`
     : `https://arsenal-fc-project-348a.vercel.app/${PREVIEW_PATH}`;
 
 
-const getPreviewUrl = (doc: SanityDocument ) => {
-  if (!doc.accessibleSlug?.current) return `${siteUrl}?id=${doc._id}`;
-  console.log(doc.accessibleSlug?.current)
-  console.log(doc)
+const PREVIEW_PATH_PRODUCT = ''
 
-  return `${siteUrl}?slug=${doc?.slugPrefix ?? ''}${doc.accessibleSlug?.current}&id=${doc._id}`;
-};
+const siteUrlProduct =
+  process.env.NODE_ENV === 'development'
+    ? `http://localhost:3000/${PREVIEW_PATH_PRODUCT}`
+    : process.env.NEXT_PUBLIC_SANITY_DATASET === 'production'
+    ? `https://arsenal-fc-project-348a.vercel.app/${PREVIEW_PATH_PRODUCT}` 
+    : `https://arsenal-fc-project-348a.vercel.app/${PREVIEW_PATH_PRODUCT}`;
+
+
+
+    const getPreviewUrl = (doc: SanityDocument) => {
+      if(doc._type === 'home') return `${siteUrl}?id=${doc._id}`;
+
+      if(doc._type === 'sitePage' && doc.accessibleSlug?.current === 'home') return `${siteUrl}?id=${doc._id}`;
+
+      if (doc._type === 'siteConfig') return `${siteUrl}?id=${doc._id}`;
+
+      if (!doc.slug?.current) {
+        return  `${siteUrl}?slug=${doc?.slugPrefix ?? ''}${doc.accessibleSlug?.current}&id=${doc._id}`;
+      }
+      
+      if (!doc.accessibleSlug?.current) {
+        return `${siteUrlProduct}${doc._type}s/${doc.slug.current}`;
+      }
+    
+      const slugPrefix = doc.slug ? doc.slug.current : ''; // Retrieve the slug prefix from the document
+      const accessibleSlug = doc.accessibleSlug ? doc.accessibleSlug.current : '';
+    
+      return `${siteUrl}?slug=${slugPrefix}${accessibleSlug}&id=${doc._id}`;
+    };
+    
 
 export const defaultDocumentNode: DefaultDocumentNodeResolver = (S, { schemaType }) => {
   switch (schemaType) {
@@ -65,6 +92,39 @@ export const defaultDocumentNode: DefaultDocumentNodeResolver = (S, { schemaType
         })
         .title('Preview'),
       ]);
+
+      case `product`:
+      return S.document().views([
+        S.view.form(),
+        S.view
+        .component(Iframe)
+        .options({
+          url: (doc: SanityDocument) => getPreviewUrl(doc),
+        })
+        .title('Preview'),
+      ]);
+
+      case `sitePage`:
+      return S.document().views([
+        S.view.form(),
+        S.view
+        .component(Iframe)
+        .options({
+          url: (doc: SanityDocument) => getPreviewUrl(doc),
+        })
+        .title('Preview'),
+      ]);
+
+      case `siteConfig`:
+        return S.document().views([
+          S.view.form(),
+          S.view
+          .component(Iframe)
+          .options({
+            url: (doc: SanityDocument) => getPreviewUrl(doc),
+          })
+          .title('Preview'),
+        ]);
       
     default:
       return S.document().views([S.view.form()]);
